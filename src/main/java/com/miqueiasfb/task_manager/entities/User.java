d4aa8206c93c92bdff01bfd0eaa2590ebfe4c1fe
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 16:35:19 -0300

    added entity User

---
+++ src/main/java/com/miqueiasfb/task_manager/entities/User.java
@@ -0,0 +1,30 @@
+package com.miqueiasfb.task_manager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  @NotBlank(message = "E-mail is mandatory")
+  @Email(message = "E-mail is invalid")
+  private String email;
+
+  @NotBlank(message = "Password is mandatory")
+  private String password;
+}

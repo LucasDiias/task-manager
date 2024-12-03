e1bd12b4945aacee891f633b849177cf32e72349
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 13:29:36 -0300

    update pom.xml

--- src/main/java/com/miqueiasfb/task_manager/repositories/TaskRepository.java
+++ src/main/java/com/miqueiasfb/task_manager/repositories/TaskRepository.java
@@ -1,9 +1,11 @@
 package com.miqueiasfb.task_manager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.miqueiasfb.task_manager.entities.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }

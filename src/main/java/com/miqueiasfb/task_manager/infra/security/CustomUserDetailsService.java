b0c8a2089b59c73c2f4f872efc5027b12acdc786
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 20:31:45 -0300

    added @Component

--- src/main/java/com/miqueiasfb/task_manager/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/miqueiasfb/task_manager/infra/security/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.miqueiasfb.task_manager.entities.User;
 import com.miqueiasfb.task_manager.repositories.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
   @Autowired
   private UserRepository userRepository;

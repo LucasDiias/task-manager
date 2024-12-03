16dc0465e2a91ae3f12822e5fc92b79ca2073e9f
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 01:31:25 -0300

    created entity Todo(id, title, description, priority, done, doneAt, createdAt)

--- src/main/java/com/miqueiasfb/task_manager/entities/Task.java
+++ src/main/java/com/miqueiasfb/task_manager/entities/Task.java
@@ -16,8 +16,65 @@ public class Task {
   private Long id;
   private String title;
   private String description;
-  private boolean done = false;
-  private LocalDate createdAt = LocalDate.now();
-  private LocalDate doneAt;
   private int priority;
+  private boolean done = false;
+  private LocalDate doneAt;
+  private LocalDate createdAt = LocalDate.now();
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getPriority() {
+    return priority;
+  }
+
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+
+  public LocalDate getDoneAt() {
+    return doneAt;
+  }
+
+  public void setDoneAt(LocalDate doneAt) {
+    this.doneAt = doneAt;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDate createdAt) {
+    this.createdAt = createdAt;
+  }
+
 }

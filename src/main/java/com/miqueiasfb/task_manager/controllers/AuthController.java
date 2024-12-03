bf035d808dd4d6aae59690c877b4dd16e2ee64f3
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 20:44:39 -0300

    solved password not matching

--- src/main/java/com/miqueiasfb/task_manager/controllers/AuthController.java
+++ src/main/java/com/miqueiasfb/task_manager/controllers/AuthController.java
@@ -29,7 +29,7 @@ public class AuthController {
   @PostMapping("/login")
   public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO body) {
     User user = this.userRepository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
-    if (passwordEncoder.matches(user.getPassword(), body.password())) {
+    if (passwordEncoder.matches(body.password(), user.getPassword())) {
       String token = this.tokenService.generateToken(user);
       return ResponseEntity.ok(new ResponseDTO(token));
     }

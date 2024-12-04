01866e3445724597dfbaa93054c51c13f8bb4678
Lucas Dias <maykonlucasdias@gmail.com>
2024-12-03 21:00:59 -0300

    added @SuppresWarnings('unused')

--- src/main/java/com/miqueiasfb/task_manager/infra/security/SecurityConfig.java
+++ src/main/java/com/miqueiasfb/task_manager/infra/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @SuppressWarnings("unused")
   @Autowired
   private CustomUserDetailsService userDetailsService;
 
